94781591fad61584b3ad42f58fa29efdcb6b152d
derimer <jderimer@gmail.com>
2025-03-06 00:40:26 +0100

    ajustements multiples css et formation

--- index.js
+++ index.js
@@ -15,13 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
   // Ajout des écouteurs pour les réalisations
   addClickListener("calculette", "./calculette/index.html");
   addClickListener("todolist", "https://derimer.github.io/ma_liste/");
-  addClickListener("valentino", "https://stephanovalentinophoto.onrender.com/");
+  addClickListener("portfolio", "./index.html"); // Updated path
   addClickListener("tictactoe", "./tic-tac-toe/index.html");
   addClickListener("tickets", "./tickets/index.html");
   addClickListener("html", "https://developer.mozilla.org/fr/docs/Web/HTML");
+  addClickListener("figma", "https://www.figma.com/");
+  addClickListener("trello", "https://trello.com/");
   addClickListener("css", "https://developer.mozilla.org/fr/docs/Web/CSS");
   addClickListener("js", "https://developer.mozilla.org/fr/docs/Web/JavaScript");
   addClickListener("react", "https://fr.react.dev/");
   addClickListener("node", "https://nodejs.org/fr/");
+  addClickListener("mysql", "https://www.mysql.com/");
+  addClickListener("github", "https://github.com/");
   addClickListener("creation3", "https://stephanovalentinophoto.com/");
 });
